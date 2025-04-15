84df790cbb30dca0aadae2406333270d45841e76
udaykrishna2k <uday.krishna@hashteksolutions.com>
2025-04-15 11:49:14 +0530

    Initial

---
+++ monitoring/src/test/java/com/hands_on/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hands_on.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
